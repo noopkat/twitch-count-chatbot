05437a0510263513f2734ee0671341dc49f8d415
Ram Pasala <ram.pasala7@gmail.com>
2017-10-28 23:53:37 +0530

    feat(enhancement):promises & interactive cli

--- server.js
+++ server.js
@@ -1,39 +1,75 @@
 require('dotenv').config();
 const IRC = require('irc-framework');
 const skateboard = require('skateboard');
+const inquirer = require('inquirer'); 
 
 const bot = new IRC.Client();
 let count = 0;
+let username = '';
+let token = '';
+let channelName = '';
 let socket = null;
 
-function botConnect(callback) {
-  bot.connect({
-    host: 'irc.chat.twitch.tv',
-    port: 6667,
-    nick: process.env.TWITCH_NICK,
-    password: process.env.TWITCH_TOKEN
-  });
+const questions = [
+  {
+    message: 'What is our twitch username?',
+    name: 'username',
+    type: 'input',
+  },
+  {
+    message: 'Please enter your twitch IRC chat token:',
+    name: 'token',
+    type: 'input',
+  },
+  {
+    message: 'Please enter the twitch channel(# followed by channel name):',
+    name: 'channel',
+    type: 'input',
+  }
 
-  bot.on('registered', () => {
-    console.log('reg happened!');
+];
 
-    const channel = bot.channel('#twitchcongiveplz');
-    channel.join();
-    return callback();
+function botConnect() {
+  return new Promise((resolve) => {
+    resolve(bot.connect({
+          host: 'irc.chat.twitch.tv',
+          port: 6667,
+          nick: username,
+          password: token
+        }));
   });
 };
-botConnect(() => {
-  console.log('joined channel!');
 
-  skateboard({port: 3000}, (stream) => {
-    socket = stream;
+function botRegister() {
+  return new Promise((resolve) => {
+    bot.on('registered', () => {
+      const channel = bot.channel(channelName);
+      channel.join();
+      resolve('\n'+ 'registration happened!' +'\n');
+    });
+  })
+};
+
+inquirer.prompt(questions).then((answers) => {
+  
+  username = answers.username;
+  token = answers.token;
+  channelName = answers.channel;
+
+  botConnect().then(() => {
+    botRegister().then((status) => {
+      console.log(status);
+      console.log('joined channel!');
+      skateboard({port: 3000}, (stream) => {
+        socket = stream;
+      });
+    
+      bot.matchMessage(/^\d\d?$/, (event) => {
+        const n = parseInt(event.message);
+        count = (n - count === 1) ? n : 0;
+        socket.write(count.toString()); 
+      });
+    })
   });
 
-  bot.matchMessage(/^\d\d?$/, (event) => {
-    const n = parseInt(event.message);
-    count = (n - count === 1) ? n : 0;
-    socket.write(count.toString()); 
-  });
 });
-
-
